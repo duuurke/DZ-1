1428fc1d6a6a6ba945b94d5adbb1cf5f0f7bf9cb
unknown <you@example.com>
2024-09-04 16:04:40 +0400

    new

--- lesson_3/Mailing.py
+++ lesson_3/Mailing.py
@@ -1,9 +1,10 @@
 from Address import Address
 
+
 class Mailing:
-    def __init__(self, to_address, from_address, cost, track):
+    def __init__(self, to_address: Address,
+                 from_address: Address, cost, track):
         self.to_ad = to_address
         self.from_ad = from_address
         self.cost = cost
         self.track = track
-
--- lesson_3/lesson_3_task_3.py
+++ lesson_3/lesson_3_task_3.py
@@ -1,11 +1,11 @@
 from Address import Address
 from Mailing import Mailing
 
-to_ad = Address('148863', 'Астрахань', 'Зеленгинская', '15', '22')
+to_ad = Address('238863', 'Астрахань', 'Зеленгинская', '15', '22')
 
-from_ad = Address('128873', 'п-о Крым', 'Астраханская', '27', '11')
+from_ad = Address('778873', 'п-о Крым', 'Астраханская', '27', '11')
 
-Mailing = Mailing(to_ad, from_ad, '11:00', '15 000')
+Mailing = Mailing(to_ad, from_ad, '15000', '000120030')
 
-print(f' отправление {Mailing.to_ad} из {Mailing.from_ad}'
-                           f' в {Mailing.cost}. стоимость {Mailing.track} рублей.')
+print(f'отправление {Mailing.track} из {Mailing.to_ad} в {Mailing.from_ad}'
+      f' Cтоимость {Mailing.cost} рублей.')
