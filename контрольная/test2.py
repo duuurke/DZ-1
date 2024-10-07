fb9080dd560cb2af5e27bb9c051cb2082e2ebea5
unknown <you@example.com>
2024-10-08 02:23:34 +0400

    new

--- контрольная/test2.py
+++ контрольная/test2.py
@@ -3,28 +3,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from data import *
-from time import sleep 
+
+def test_2():
+    driver = webdriver.Firefox()
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+
+    pole = driver.find_element(By.ID, 'delay')
+    pole.clear()
+    pole.send_keys('45')
+
+    driver.find_element(By.XPATH, "//span[text() = '7']").click()
+    driver.find_element(By.XPATH, "//span[text() = '+']").click()
+    driver.find_element(By.XPATH, "//span[text() = '8']").click()
+    driver.find_element(By.XPATH, "//span[text() = '=']").click()
 
 
-driver = webdriver.Firefox()
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
-
-pole = driver.find_element(By.ID, 'delay')
-pole.clear()
-pole.send_keys('45')
-
-driver.find_element(By.XPATH, "//span[text() = '7']").click()
-driver.find_element(By.XPATH, "//span[text() = '+']").click()
-driver.find_element(By.XPATH, "//span[text() = '8']").click()
-driver.find_element(By.XPATH, "//span[text() = '=']").click()
-sleep (1)
-
-WebDriverWait(driver, 50).until(
+    WebDriverWait(driver, 50).until(
     EC.text_to_be_present_in_element((By.CLASS_NAME, "screen"), '15'))
-txt = driver.find_element(By.CLASS_NAME, "screen").text
 
-def test():
- assert txt =='15'
-sleep(5)
+    txt = driver.find_element(By.CLASS_NAME, "screen").text
 
-driver.quit()
+
+    assert txt =='15'
+
+    driver.quit()
--- контрольная/test3.py
+++ контрольная/test3.py
@@ -5,26 +5,28 @@ from selenium.webdriver.support import expected_conditions as EC
 from data import *
 from time import sleep 
 
+def test_3():
+    driver = webdriver.Firefox()
+    driver.get("https://www.saucedemo.com/")
 
-driver = webdriver.Firefox()
-driver.get("https://www.saucedemo.com/")
+    driver.find_element(By.ID, 'user-name').send_keys('standard_user')
+    driver.find_element(By.ID, 'password').send_keys('secret_sauce')
+    driver.find_element(By.ID, 'login-button').click()
+    driver.find_element(By.ID, 'add-to-cart-sauce-labs-backpack').click()
+    driver.find_element(By.ID, 'add-to-cart-sauce-labs-bolt-t-shirt').click()
+    driver.find_element(By.ID, 'add-to-cart-sauce-labs-onesie').click()
+    driver.find_element(By.ID, 'shopping_cart_container').click()
+    driver.find_element(By.ID, 'checkout').click()
+    driver.find_element(By.ID, 'first-name').send_keys('инокентий')
+    driver.find_element(By.ID, 'last-name').send_keys('трактарин')
+    driver.find_element(By.ID, 'postal-code').send_keys('1489456')
+    driver.find_element(By.ID, 'continue').click()
 
-driver.find_element(By.ID, 'user-name').send_keys('standard_user')
-driver.find_element(By.ID, 'password').send_keys('secret_sauce')
-driver.find_element(By.ID, 'login-button').click()
-driver.find_element(By.ID, 'add-to-cart-sauce-labs-backpack').click()
-driver.find_element(By.ID, 'add-to-cart-sauce-labs-bolt-t-shirt').click()
-driver.find_element(By.ID, 'add-to-cart-sauce-labs-onesie').click()
-driver.find_element(By.ID, 'shopping_cart_container').click()
-driver.find_element(By.ID, 'checkout').click()
-driver.find_element(By.ID, 'first-name').send_keys('инокентий')
-driver.find_element(By.ID, 'last-name').send_keys('трактарин')
-driver.find_element(By.ID, 'postal-code').send_keys('1489456')
-driver.find_element(By.ID, 'continue').click()
+    p = driver.find_element(By.CLASS_NAME, 'summary_total_label')
+    total = p.text.strip().replace("Total: $", '')
 
-p = driver.find_element(By.CLASS_NAME, 'summary_total_label')
-total = p.text.strip().replace("total: $", '')
+    ex_total = '58.29'
+    assert total == ex_total 
+    print (f'итоговая сумма ${total}')
 
-ex_total = '58.29'
-assert total == ex_total 
-print ('fитоговая сумма ${total}')
+    driver.quit()
