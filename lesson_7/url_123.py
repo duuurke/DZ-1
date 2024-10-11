108e0ea613fdd86223f7632a45ad116eee0f07e6
duuurke <165960286+duuurke@users.noreply.github.com>
2024-10-11 17:38:01 +0400

    Delete lesson_7 directory

--- lesson_7/url_123.py
+++
@@ -1,3 +0,0 @@
-forma_url = "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
-calculator_url = "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
-shop_url = "https://www.saucedemo.com/"
