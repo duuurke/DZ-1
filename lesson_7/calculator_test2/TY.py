a7eab290a6394a1d96715aa79f8f1173f8361eef
unknown <you@example.com>
2024-10-11 17:50:59 +0400

    new

--- lesson_7/calculator_test2/TY.py
+++ lesson_7/calculator_test2/TY.py
@@ -13,18 +13,18 @@ class TY():
         self.driver.get (calculator_url)
         
 
-    def calculator(self):#изменить значение "5"
+    def calculator(self):#изменить значение на "5"
         pole = self.driver.find_element(By.ID, "delay")
         pole.clear()
         pole.send_keys('1')
 
-    def fill (self):#ввести данные в кадькулятор и нажать кнопку '='
+    def fill (self):#ввести данные в кадькулятор и нажать кнопку равно '='
         self.driver.find_element(By.XPATH, "//span[text() = '7']").click()
         self.driver.find_element(By.XPATH, "//span[text() = '+']").click()
         self.driver.find_element(By.XPATH, "//span[text() = '8']").click()
         self.driver.find_element(By.XPATH, "//span[text() = '=']").click()
 
-    def click_but(self):#убедиться что получаемое число равно "15"
+    def click_but(self):#убедиться что число равно "15"
         WebDriverWait(self.driver, 50).until(
         EC.text_to_be_present_in_element((By.CLASS_NAME, "screen"), '15'))
         return self.driver.find_element(By.CLASS_NAME, "screen").text
