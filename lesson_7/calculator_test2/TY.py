b1bffe30f043d300e0b526f695a124e5b0e32492
unknown <you@example.com>
2024-10-13 23:59:55 +0400

    new

---
+++ lesson_7/calculator_test2/TY.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from lesson_7.url_123 import calculator_url
+
+
+class TY():
+
+    def __init__(self, driver):
+        #driver = webdriver.Firefox()
+        self.driver = driver
+        self.driver.get (calculator_url)
+        
+
+    def calculator(self, delay):#изменить значение на "45"
+        pole = self.driver.find_element(By.ID, "delay")
+        pole.clear()
+        pole.send_keys(delay)
+
+    def fill(self, argum):#ввести данные в кадькулятор и нажать кнопку равно '='
+        self.driver.find_element(By.XPATH, f"//span[text() = '{argum}']").click()
+
+    def click_but(self):#убедиться что число равно "15"
+        WebDriverWait(self.driver, 50).until(
+        EC.text_to_be_present_in_element((By.CLASS_NAME, "screen"), '15'))
+        return self.driver.find_element(By.CLASS_NAME, "screen").text
---
+++ lesson_7/test_calculator.py
@@ -0,0 +1,14 @@
+from lesson_7.calculator_test2.TY import TY
+from selenium import webdriver
+
+def test2():
+    driver = webdriver.Firefox()
+    calc = TY(driver)
+    calc.calculator(45)
+    calc.fill('7')
+    calc.fill('+')
+    calc.fill('8')
+    calc.fill('=')
+    assert "15" in calc.click_but()
+
+    calc.driver.quit()
